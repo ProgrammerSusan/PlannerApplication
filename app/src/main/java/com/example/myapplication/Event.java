62749b05c80ab43c4f05a04bd98349eb29f2ca01
sarahyaw <syaw@emich.edu>
2020-04-12 10:09:39 -0400

    option page is set up

--- app/src/main/java/com/example/myapplication/Event.java
+++ app/src/main/java/com/example/myapplication/Event.java
@@ -4,10 +4,14 @@ public class Event
 {
     public double time;
     public String eventName;
+    public String description;
+    public static int color;
+    public static double alertTime;
 
-    public Event(double Time, String event)
+    public Event(double Time, String event, String desc)
     {
         time = Time;
         eventName = event;
+        description = desc;
     }
 }
--- app/src/main/java/com/example/myapplication/Options.java
+++ app/src/main/java/com/example/myapplication/Options.java
@@ -2,13 +2,58 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.RadioButton;
 
-public class Options extends AppCompatActivity {
+public class Options extends AppCompatActivity
+{
+    public RadioButton red, orange, yellow, green, blue, indego, violet;
+    public RadioButton halfHour, hour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+    }
+
+    public void timeOnClick(View view)
+    {
+        halfHour = (RadioButton) view;
+        hour = (RadioButton) view;
+
+        if(halfHour.isChecked())
+            Event.alertTime = .5;
+        else
+            Event.alertTime = 1;
+    }
+
+    public void colorOnClick(View view)
+    {
+        red = (RadioButton) view;
+        orange = (RadioButton) view;
+        yellow = (RadioButton) view;
+        green = (RadioButton) view;
+        blue = (RadioButton) view;
+        indego = (RadioButton) view;
+        violet = (RadioButton) view;
+
+        if(red.isChecked())
+            Event.color = Color.parseColor("#AE4040");
+        else if (orange.isChecked())
+            Event.color = Color.parseColor("#BD7E00");
+        else if (yellow.isChecked())
+            Event.color = Color.parseColor("#C1BF22");
+        else if (green.isChecked())
+            Event.color = Color.parseColor("#157126");
+        else if (blue.isChecked())
+            Event.color = Color.parseColor("#155B71");
+        else if (indego.isChecked())
+            Event.color = Color.parseColor("#271571");
+        else
+            Event.color = Color.parseColor("#3A0D56");
     }
 }
