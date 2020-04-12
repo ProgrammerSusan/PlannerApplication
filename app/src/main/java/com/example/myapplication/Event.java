ce20c51b33a5acfe5594a99eb0299ef1ff664c01
sarahyaw <syaw@emich.edu>
2020-04-11 20:02:56 -0400

    rough layout outline

---
+++ app/src/main/java/com/example/myapplication/Event.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+public class Event
+{
+    public double time;
+    public String eventName;
+
+    public Event(double Time, String event)
+    {
+        time = Time;
+        eventName = event;
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    //set interface for event lists here
 }
